242c1794c951b88e0eb0c660738dc4beeba84491
unknown <examplse@mail.com>
2022-04-28 22:21:12 +0400

    header changed

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return  <nav className="#7e57c2 deep-purple lighten-1">
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Movies</a>
+      <a href="!#" className="brand-logo min-link">React Movies</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="!#">Repo</a></li>
       </ul>
